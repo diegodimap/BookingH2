b770a952e120b685450b67f205b1942affbc191c
Diego Oliveira <diegodimap@gmail.com>
2023-11-09 07:39:43 -0300

    Initial commit

---
+++ src/main/java/com/itprofessor/bookingh2/BookingH2Application.java
@@ -0,0 +1,13 @@
+package com.itprofessor.bookingh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/itprofessor/bookingh2/BookingH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itprofessor.bookingh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
