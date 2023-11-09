a8de6fcc6f52e296f3e1ddb38a7291e4169d5c0f
Diego Oliveira <diegodimap@gmail.com>
2023-11-09 10:36:14 -0300

    Overlaping booking check completed.

--- src/main/java/com/itprofessor/bookingh2/controller/BookingController.java
+++ src/main/java/com/itprofessor/bookingh2/controller/BookingController.java
@@ -3,6 +3,7 @@ package com.itprofessor.bookingh2.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.itprofessor.bookingh2.data.DataCheck;
 import com.itprofessor.bookingh2.dto.BookingDto;
 import com.itprofessor.bookingh2.model.Booking;
 import com.itprofessor.bookingh2.repository.BookingRepository;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -32,7 +32,7 @@ public class BookingController {
         booking.setUser_id(bookingDto.getUser_id());
         booking.setProperty_id(bookingDto.getProperty_id());
 
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
         Date startDate = format.parse(bookingDto.getStart_date());
         Date endDate = format.parse(bookingDto.getEnd_date());
 
@@ -40,10 +40,22 @@ public class BookingController {
         booking.setEnd_date(endDate);
 
         //before saving, need to verify if dates are not blocked nor overlap other bookings
+        DataCheck dataCheck = new DataCheck();
+        System.out.println(startDate);
+        System.out.println(endDate);
+        String result = dataCheck.getBookings(h2DateFormat(startDate), h2DateFormat(endDate), bookingDto.getProperty_id());
+        System.out.println(result);
 
-        repository.save(booking);
+        //overlaping booking = false
+        if(result.length() == 0) {
+            repository.save(booking);
+        }
 
         return booking.getId();
+    }
+
+    private String h2DateFormat(Date date) {
+        return (date.getYear()+1900) + "-" + (date.getMonth()+1) + "-" + date.getDate();
     }
 
     @PutMapping("/updateBooking/{id}")
@@ -53,7 +65,7 @@ public class BookingController {
         if(bookingDatabase.isPresent()) {
             bookingDatabase.get().setProperty_id(bookingDto.getProperty_id());
             bookingDatabase.get().setUser_id(bookingDto.getUser_id());
-            SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+            SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
             Date startDate = format.parse(bookingDto.getStart_date());
             Date endDate = format.parse(bookingDto.getEnd_date());
             bookingDatabase.get().setStart_date(startDate);
@@ -80,12 +92,21 @@ public class BookingController {
 
     @GetMapping("/listBookings")
     public String findAllBookings() throws JsonProcessingException {
-        Iterable<Booking> tasks = repository.findAll();
+        Iterable<Booking> bookings = repository.findAll();
 
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String json = ow.writeValueAsString(tasks);
+        String json = ow.writeValueAsString(bookings);
 
         return json;
+    }
+
+    @GetMapping("/data")
+    public String checkData() throws JsonProcessingException {
+        DataCheck dataCheck = new DataCheck();
+
+        String teste = dataCheck.getBookings("2023-11-10", "2023-11-15", 3);
+
+        return teste;
     }
 
 }
---
+++ src/main/java/com/itprofessor/bookingh2/data/DataCheck.java
@@ -0,0 +1,52 @@
+package com.itprofessor.bookingh2.data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DataCheck {
+
+    private Statement stmt;
+    private ResultSet rs;
+    public Connection conn;
+    public String resultado = "";
+
+    public DataCheck() {
+        try {
+            String url = "jdbc:h2:mem:bookings";
+            String usr = "sa";
+            String pas = "";
+
+            try {
+                conn = DriverManager.getConnection(url, usr, pas);
+                stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            } catch (Exception e) {
+                System.out.println("Error" + e.getMessage());
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(DataCheck.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        }
+    }
+
+    public String getBookings(String start_date, String end_date, int property_id){
+        String sql = "SELECT * FROM booking WHERE start_date >= '"+start_date+"' AND end_date <= '"+end_date+"' AND property_id="+property_id;
+        System.out.println(sql);
+        try{
+            rs = stmt.executeQuery(sql);
+            resultado = "";
+
+            while (rs.next()) {
+                resultado += rs.getInt("id");
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return resultado;
+    }
+
+}
